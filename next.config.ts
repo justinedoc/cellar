779fac40a8bb50d4b4ae040c094687a458895aef
Onyiriuka Justin <richardjustin500@gmail.com>
2025-07-06 11:49:57 +0100

    app config setup complete

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule: any) =>
+        rule &&
+        typeof rule === "object" &&
+        rule.test instanceof RegExp &&
+        rule.test.test(".svg")
+    );
+
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "component",
+      },
+    },
+  },
 };
 
 export default nextConfig;
