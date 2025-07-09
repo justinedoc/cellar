b40d91ece53c979e0d05af8907adb29d79bb441f
Onyiriuka Justin <richardjustin500@gmail.com>
2025-07-09 18:41:14 +0100

    [fix] minor theme fix

--- src/components/landing/navbar.tsx
+++ src/components/landing/navbar.tsx
@@ -6,7 +6,7 @@ import NavMenu from "./nav-menu";
 
 function Navbar() {
   return (
-    <nav className="fixed left-1/2 z-[100] mt-2 flex w-full max-w-[85%] -translate-x-1/2 items-center justify-between rounded-2xl border px-2 py-1.5 backdrop-blur-2xl">
+    <nav className="fixed left-1/2 z-[100] mt-2 flex w-full -translate-x-1/2 items-center justify-between rounded-2xl border px-2 py-1.5 backdrop-blur-2xl md:max-w-[85%] 2xl:max-w-[65%]">
       <Logo />
       <NavMenu />
 
