74ff2d0a4a63bea5b6d04f95540e002d92cd3fce
Onyiriuka Justin Ifeanyi <165206080+justinedoc@users.noreply.github.com>
2025-07-14 01:29:51 +0100

    Update navbar

--- src/components/landing/navigation/index.tsx
+++ src/components/landing/navigation/index.tsx
@@ -4,7 +4,7 @@ import NavMenu from "./nav-menu";
 
 function Navbar() {
   return (
-    <nav className="fixed top-4 left-1/2 z-100 flex w-full max-w-[95%] -translate-x-1/2 justify-between rounded-md border px-2 py-2 backdrop-blur-2xl sm:max-w-[90%] md:max-w-[85%] md:items-center md:rounded-2xl md:py-1.5 2xl:max-w-[65%]">
+    <nav className="fixed top-4 left-1/2 z-100 flex w-full max-w-[95%] -translate-x-1/2 justify-between rounded-full border px-2 py-2 backdrop-blur-2xl sm:max-w-[90%] md:max-w-[85%] md:items-center md:py-1.5 2xl:max-w-[70%]">
       <Logo />
 
       <NavMenu />
